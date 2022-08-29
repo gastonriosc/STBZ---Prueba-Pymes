924e722020bdd7bd11c46158dd0e13253928a9e2
gastonriosc <gastonriosc@gmail.com>
2022-08-29 17:02:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-dgxqq1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-dgxqq1)
---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ArticulosFamiliasComponent } from './components/articulos-familias/articulos-familias.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ArticulosComponent } from './components/articulos/articulos.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    NgbPaginationModule,
+
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+      { path: 'inicio', component: InicioComponent },
+      { path: 'articulosfamilias', component: ArticulosFamiliasComponent },
+      { path: 'articulos', component: ArticulosComponent },
+    ]),
+  ],
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    ArticulosFamiliasComponent,
+    MenuComponent,
+    ArticulosComponent,
+  ],
+
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
+
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
